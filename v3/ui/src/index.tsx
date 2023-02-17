f275536e982fa2f0334a06596d75da0b62a558aa
Noisekit <nikita@cc.snxdao.io>
2023-02-17 21:09:14 +0800

    Add onboard and upgrade useBlockchain

--- v3/ui/src/index.tsx
+++ v3/ui/src/index.tsx
@@ -1,41 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import { Synthetix } from './App';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Fonts, theme } from '@synthetixio/v3-theme';
-import { BrowserRouter } from 'react-router-dom';
-import { DEFAULT_QUERY_REFRESH_INTERVAL, DEFAULT_QUERY_STALE_TIME } from '@snx-v3/Constants';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import './i18n';
-
-import { GasSpeedProvider } from '@snx-v3/useGasSpeed';
-import { BlockchainProvider } from '@snx-v3/useBlockchain';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchInterval: DEFAULT_QUERY_REFRESH_INTERVAL,
-      staleTime: DEFAULT_QUERY_STALE_TIME,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 const container = document.querySelector('#app');
-const root = createRoot(container);
-
-root.render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <Fonts />
-        <BlockchainProvider>
-          <GasSpeedProvider>
-            <Synthetix />
-          </GasSpeedProvider>
-          <ReactQueryDevtools />
-        </BlockchainProvider>
-      </ChakraProvider>
-    </QueryClientProvider>
-  </BrowserRouter>
-);
+async function run() {
+  if (!container) {
+    throw new Error('Container #app does not exist');
+  }
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+}
+run();
--- v3/ui/src/layouts/Default/NetworkController.tsx
+++ v3/ui/src/layouts/Default/NetworkController.tsx
@@ -1,130 +1,108 @@
-// import { Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
-// import { ConnectButton } from '@rainbow-me/rainbowkit';
-// import { ChevronDown, ChevronUp, EthereumIcon, OptimismIcon, WalletIcon } from '@snx-v3/icons';
-// import { getNetworkByName, useAccount } from '@snx-v3/useBlockchain';
+import {Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text} from '@chakra-ui/react';
+import {ChevronDown, ChevronUp, WalletIcon} from '@snx-v3/icons';
+import {
+  DEFAULT_NETWORK,
+  NETWORKS,
+  onboard,
+  UNSUPPORTED_NETWORK,
+  useAccount,
+  useNetwork,
+} from '@snx-v3/useBlockchain';
 
 export function NetworkController() {
-  // const connectedAccount = useAccount();
-  // const switchNetwork = (...args) => {
-  //   console.log(...args);
-  // };
-
-  return null;
+  const account = useAccount();
+  const activeNetwork = useNetwork();
+  const selectedNetwork =
+      account && activeNetwork ? activeNetwork : !account ? DEFAULT_NETWORK : UNSUPPORTED_NETWORK;
+  return (
+      <Flex>
+        <Menu>
+          {({isOpen}) => (
+              <>
+                <MenuButton
+                    as={Button}
+                    variant="outline"
+                    colorScheme="gray"
+                    sx={{'> span': {display: 'flex', alignItems: 'center'}}}
+                    mr={1}
+                >
+                  <selectedNetwork.Icon />
+                  <Text
+                      variant="nav"
+                      fontSize="sm"
+                      fontWeight={700}
+                      ml={1.5}
+                      mr={2}
+                      display={{base: 'none', md: 'initial'}}
+                  >
+                    {selectedNetwork.displayName}
+                  </Text>
+                  <Flex display={{base: 'none', md: 'initial'}}>
+                    {isOpen ? <ChevronUp color="cyan" /> : <ChevronDown color="cyan.500" />}
+                  </Flex>
+                </MenuButton>
+                <MenuList>
+                  {Object.values(NETWORKS).map((network) => (
+                      <MenuItem
+                          key={network.name}
+                          disabled={!network.isSupported}
+                          onClick={async () => {
+                            const success = await onboard.setChain({
+                              chainId: `0x${network.id.toString(16)}`,
+                            });
+                            console.log({success});
+                          }}
+                      >
+                        <network.Icon />
+                        <Text variant="nav" ml={2}>
+                          {network.displayName}
+                        </Text>
+                      </MenuItem>
+                  ))}
+                </MenuList>
+              </>
+          )}
+        </Menu>
+        {account ? (
+            <Button
+                variant="outline"
+                colorScheme="gray"
+                ml={2}
+                height={10}
+                py="6px"
+                px="9.5px"
+                onClick={() => {
+                  // if (chain?.unsupported) {
+                  //   openChainModal();
+                  // } else {
+                  //   openAccountModal();
+                  // }
+                }}
+            >
+              <WalletIcon />
+              <Text ml={1} color="whiteAlpha.800" fontWeight={700} fontSize="xs" userSelect="none">
+                {/*{account?.ensName || account.displayName}*/}
+              </Text>
+            </Button>
+        ) : (
+            <Button
+                onClick={() => onboard.connectWallet()}
+                type="button"
+                size="sm"
+                ml={2}
+                py={5}
+                data-testid="connect-wallet-button"
+            >
+              Connect Wallet
+            </Button>
+        )}
+      </Flex>
+  );
 
   // return (
   //   <ConnectButton.Custom>
   //     {({ account, chain, openAccountModal, openChainModal, openConnectModal, mounted }) => {
   //       const network = getNetworkByName(chain?.name);
-  //       return (
-  //         <Flex
-  //           {...(!mounted && {
-  //             'aria-hidden': true,
-  //             style: {
-  //               opacity: 0,
-  //               pointerEvents: 'none',
-  //               userSelect: 'none',
-  //             },
-  //           })}
-  //         >
-  //           <Menu>
-  //             {({ isOpen }) => (
-  //               <>
-  //                 <MenuButton
-  //                   as={Button}
-  //                   variant="outline"
-  //                   colorScheme="gray"
-  //                   sx={{ '> span': { display: 'flex', alignItems: 'center' } }}
-  //                   mr={1}
-  //                 >
-  //                   <network.Icon />
-  //
-  //                   <Text
-  //                     variant="nav"
-  //                     fontSize="sm"
-  //                     fontWeight={700}
-  //                     ml={1.5}
-  //                     mr={2}
-  //                     display={{ base: 'none', md: 'initial' }}
-  //                   >
-  //                     {network.displayName}
-  //                   </Text>
-  //                   <Flex display={{ base: 'none', md: 'initial' }}>
-  //                     {isOpen ? <ChevronUp color="cyan" /> : <ChevronDown color="cyan.500" />}
-  //                   </Flex>
-  //                 </MenuButton>
-  //                 <MenuList>
-  //                   <MenuItem
-  //                     onClick={() => {
-  //                       if (connectedAccount?.address && switchNetwork) {
-  //                         switchNetwork(1);
-  //                       } else {
-  //                         openConnectModal();
-  //                       }
-  //                     }}
-  //                   >
-  //                     <EthereumIcon />
-  //                     <Text variant="nav" ml={2}>
-  //                       Ethereum Mainnet
-  //                     </Text>
-  //                   </MenuItem>
-  //                   <MenuItem
-  //                     onClick={() => {
-  //                       if (connectedAccount?.address && switchNetwork) {
-  //                         switchNetwork(10);
-  //                       } else {
-  //                         openConnectModal();
-  //                       }
-  //                     }}
-  //                   >
-  //                     <OptimismIcon />
-  //                     <Text variant="nav" ml={2}>
-  //                       Optimism
-  //                     </Text>
-  //                   </MenuItem>
-  //                 </MenuList>
-  //               </>
-  //             )}
-  //           </Menu>
-  //           {connectedAccount?.address && account && mounted ? (
-  //             <Button
-  //               variant="outline"
-  //               colorScheme="gray"
-  //               ml={2}
-  //               height={10}
-  //               py="6px"
-  //               px="9.5px"
-  //               onClick={() => {
-  //                 if (chain?.unsupported) {
-  //                   openChainModal();
-  //                 } else {
-  //                   openAccountModal();
-  //                 }
-  //               }}
-  //             >
-  //               <WalletIcon />
-  //               <Text
-  //                 ml={1}
-  //                 color="whiteAlpha.800"
-  //                 fontWeight={700}
-  //                 fontSize="xs"
-  //                 userSelect="none"
-  //               >
-  //                 {account?.ensName || account.displayName}
-  //               </Text>
-  //             </Button>
-  //           ) : (
-  //             <Button
-  //               onClick={openConnectModal}
-  //               type="button"
-  //               size="sm"
-  //               ml={2}
-  //               py={5}
-  //               data-testid="connect-wallet-button"
-  //             >
-  //               Connect Wallet
-  //             </Button>
-  //           )}
-  //         </Flex>
   //       );
   //     }}
   //   </ConnectButton.Custom>
